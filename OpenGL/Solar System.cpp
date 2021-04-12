fd269994d5fc2291a16c2d838942f6ede40c13e3
Kali Nash <kbnash@coe.edu>
2021-04-11 19:07:21 -0500

    Added description of assignment

---
+++ OpenGL/Solar System.cpp
@@ -0,0 +1,19 @@
+/*User-Interaction
+
+Add the following user-interactions:  
+
+Left  mouse-based menu
+lights
+global ambient light:  on (default), off
+positional light 1:  on (default), off 
+positional light 2:  on, off (default)
+directional light:  on, off (default)  -- optional
+
+Right mouse-based menu
+display
+mode:  wireframe, filled – flat shading (default), filled – smooth shading
+On keyboard button ‘+’  rotate through the three levels of detail
+viewing: use polar coordinates to allow the viewer to move their viewpoint along a sphere surrounding the building. When zooming in, the LOD should switch automatically.
+
+As before, please add a star ‘*’ after the currently selected mouse-based menu option as feedback to the user.  
+*/
